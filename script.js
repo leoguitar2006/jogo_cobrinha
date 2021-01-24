f08e8d3074aebe6a72b7e455af0a33abd3ca185f
Marcos Leonardo Martins <mleonardomartins@gmail.com>
2021-01-23 22:34:32 -0300

    Cobrinha criada e andando

--- script.js
+++ script.js
@@ -1,10 +1,69 @@
 let canvas = document.getElementById("snake");
 let contexto = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+let direction = "right";
 
 function criarBG() {
     contexto.fillStyle = "lightgreen";
     contexto.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-criarBG();
+function criarSnake() {
+    for (i=0; i < snake.length; i++) {
+        contexto.fillStyle = "green";
+        contexto.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+document.addEventListener("keydown", update);
+
+function update (event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function iniciarJogo() {   
+    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+    if (snake[0].x < 0 * box && direction == "left") snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y < 0 * box && direction == "up") snake[0].y = 16 * box;
+
+    criarBG();
+    criarSnake();   
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if (direction == "right") {
+        snakeX += box;
+    }
+    if (direction == "left") {
+        snakeX -= box;
+    }
+    if (direction == "up") {
+        snakeY -= box;
+    }
+    if (direction == "down") {
+        snakeY += box;
+    }
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead); /* Add um elemento no começo do array !*/
+}
+
+let jogo = setInterval(iniciarJogo ,100);
